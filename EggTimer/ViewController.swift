fe37e5c7c5021d18d472c273fd47964fe9314803
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2024-03-27 22:17:03 +0900

    updateTimer の追加

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,14 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
-
+    let eggTimes = ["Soft": 3, "Medium": 420, "Hard": 720]
+    
+    var secondsRemaining = 60
+    
+    var timer = Timer()
+    
+    @IBOutlet weak var topTitle: UILabel!
+    
     @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        timer.invalidate()
+        
         let hardness = sender.currentTitle!
         
-        let result = eggTimes[hardness]!
+        secondsRemaining = eggTimes[hardness]!
         
-        print(result)
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimer() {
+        if secondsRemaining > 0 {
+            print("\(secondsRemaining) seconds remaining")
+            secondsRemaining -= 1
+        } else {
+            timer.invalidate()
+            topTitle.text = "Done!"
+        }
     }
     
 }
